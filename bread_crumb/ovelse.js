afe48265a5980cf587fb50d8e233ac72a376eab5
leamhkea <lehe0001@stud.kea.dk>
2025-02-04 10:24:13 +0100

    tilføjet kommentarer

--- bread_crumb/ovelse.js
+++ bread_crumb/ovelse.js
@@ -1,11 +1,11 @@
-//Her er alle tre links og navne på hvidevarer
+//Her er alle tre navne og to links på hvidevarer
 const bc = [
   { name: "Hvidevarer", link: "/hvidevarer" },
   { name: "Vaskemaskiner", link: "/hvidevarer/vaskemaskiner" },
   { name: "Bosch" },
 ];
 
-//Her hidden jeg elementer, når der klikkes på button
+//Her bestemmes, at breadcrumb skal vises, når der klikkes på button
 document.querySelector("button").addEventListener("click", showBread);
 
 function showBread() {
@@ -14,9 +14,13 @@ function showBread() {
     console.log("vare", vare.link);
     console.log("********");
 
+    // Hvis det sidste elemnt (bc.length-1) skal kun være same
     if (bc[bc.length - 1].name === vare.name) {
       document.querySelector("ul").innerHTML += `<li>${vare.name}</li>`;
-    } else {
+      console.log("vi er igang med det sidste element");
+    }
+    // Ellers alle andre elementer ska være name og link
+    else {
       document.querySelector(
         "ul"
       ).innerHTML += `<li><a href= "">${vare.name}</a>&nbsp/</li>`;
@@ -24,4 +28,4 @@ function showBread() {
   });
 }
 
-console.log(bc[bc.length - 1].name);
+// &nbsp = no breaking space
