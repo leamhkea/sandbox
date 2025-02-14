93ddcc2056579053644bf298576efe725571860d
leamhkea <lehe0001@stud.kea.dk>
2025-02-14 09:24:58 +0100

    FÆRDIG

--- animalbase_filter/animalbase.js
+++ animalbase_filter/animalbase.js
@@ -103,14 +103,24 @@ function selectSort(event) {
   const sortBy = event.target.dataset.sort;
   const sortDir = event.target.dataset.sortDirection;
 
+  //Find "old" sortBy element and remove .sortBy
+  const oldElement = document.querySelector(`[data-sort="${settings.sortBy}"]`);
+  oldElement.classList.remove("sortby");
+
+  //Indicate active sort
+  event.target.classList.add("sortby");
+
   //Toggle the direction!
   if (sortDir === "asc") {
     event.target.dataset.sortDirection = "desc";
   } else {
     event.target.dataset.sortDirection = "asc";
   }
-  console.log(`User selected ${sortBy}` - `${sortDir}`);
+  console.log(`User selected ${sortBy} - ${sortDir}`);
   setSort(sortBy, sortDir);
+
+  // Tilføjet for at opdatere listen
+  // buildList();
 }
 
 function setSort(sortBy, sortDir) {
